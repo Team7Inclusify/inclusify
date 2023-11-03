7e69e4a46150b8a384fc419ffc89b78a4b2320c2
Bryan <bryanmartinez2401@gmail.com>
2023-11-02 22:14:06 -0400

    First commit

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar-whole">
+      <div className="navbar-homeLink">
+        <img className="navbar-homeImage" /> Inclusify
+      </div>
+    </div>
+  );
+}
