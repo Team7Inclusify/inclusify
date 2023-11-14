4418051c494354b3a723aaf41a6bfd7fd15c7e9e
Bryan <bryanmartinez2401@gmail.com>
2023-11-14 10:24:08 -0500

    testing stuff

--- src/pages/TempAddVideo/TempAddVideo.js
+++ src/pages/TempAddVideo/TempAddVideo.js
@@ -3,15 +3,30 @@ import "./TempAddVideo.css";
 import Files from "react-files";
 import upload_icon from "../../images/upload.png";
 import { useState } from "react";
+import ReactPlayer from "react-player";
+import { amazonS3Config } from "../../KEYS";
 
 //  This will be used to test AmazonS3 stuff,
 //  and ultimatley connecting it to Firebase
 
 export default function TempAddVideo() {
-  const [file, setFile] = useState([]);
-  const handleChange = (newFile) => {
-    console.log(newFile);
+  const [file, setFile] = useState();
+  const [videoURL, setVideoURL] = useState(null);
+
+  const handleChange = (newFiles) => {
+    const newFile = newFiles[0]; // Assuming you only handle one file at a time
+
+    console.log(JSON.stringify(newFile));
     setFile(newFile);
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      setVideoURL(event.target.result);
+    };
+
+    if (newFile instanceof Blob) {
+      reader.readAsDataURL(newFile);
+    }
   };
 
   const handleError = (error, newFile) => {
@@ -28,7 +43,7 @@ export default function TempAddVideo() {
         className="files-dropzone"
         onChange={handleChange}
         onError={handleError}
-        accepts={["image/png", ".pdf", "video/mp4"]}
+        accepts={["video/mp4", "video/x-m4v", "video/*"]}
         multiple={false}
         maxFiles={1}
         maxFileSize={100000000000}
@@ -38,6 +53,9 @@ export default function TempAddVideo() {
         <img src={upload_icon} alt="Upload" className="upload-icon" />
         Drop files here or click to upload
       </Files>
+      {videoURL && (
+        <ReactPlayer url={videoURL} width="400px" height="auto" controls />
+      )}
     </>
   );
 }
