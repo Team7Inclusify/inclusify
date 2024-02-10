da79a6345e4ed8aacc9d8bfb709eb94fcb91fa1d
Myesha <maisha.mahjabin27@gmail.com>
2024-02-10 00:37:09 -0500

    upadted

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,23 +1,41 @@
 import React from "react";
 import "./AboutUs.css";
-import inlcusify_logo from "../../images/inclusify_with_name.png";
+//import inclusify_logo from "../../images/inclusify_with_name.png";
+import Myesha_image from "../../images/Myesha.jpg";
+import Bryan_image from "../../images/Bryan.jpg";
+import Najia_image from "../../images/Najia.jpg";
+
 
 export default function AboutUs() {
   return (
     <div className="about-us-page">
-      <img
-        className="au-inclusify-logo"
-        src={inlcusify_logo}
-        alt={"Inclusify"}
-      />
-      <div className="about-us-description">
-        <h2>Who We Are</h2>
-        <p className="description-text">
-          Inclusify's aim is to focused on empowering autistic individuals and other disabled job seekers. Our platform provides a supportive environment where users can connect, share experiences, access various resources, and build their non-traditional video resumes with accessible features.
+      <div className="header">
+        
+        <h1 className="heading">Who We Are</h1>
+      </div>
+      <div className="content">
+        <p className="description">
+          Inclusify is dedicated to empowering autistic individuals and other disabled job seekers. Our platform fosters a supportive environment where users can connect, share experiences, access resources, and build non-traditional video resumes with accessible features.
         </p>
-        <p className="description-text">
-          By fostering a sense of belonging and inclusivity within the autism community and beyond, Inclusify aims to break down barriers to employment and create opportunities for individuals with disabilities to thrive in the workforce.
-        </p>
+        
+      </div>
+      <div className="team-section">
+        <div className="team-member">
+          <img src={Myesha_image} alt="Myesha" className="team-member-image" />
+          <h3 className="member-name">Myesha Mahazabeen</h3>
+          <p className="member-role">Software Engineer</p>
+          
+        </div>
+        <div className="team-member">
+          <img src={Bryan_image} alt="Bryan" className="team-member-image" />
+          <h3 className="member-name">Bryan Martinez</h3>
+          <p className="member-role">Software Engineer</p>
+        </div>
+        <div className="team-member">
+          <img src={Najia_image} alt="Najia" className="team-member-image" />
+          <h3 className="member-name">Najia Jahan</h3>
+          <p className="member-role">Software Engineer</p>
+        </div>
       </div>
     </div>
   );
