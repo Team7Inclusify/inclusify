b3c72f6ee938fe2db26d66298fffef4f2b0e15a5
bryanmartinez1 <bryanmartinez2401@gmail.com>
2024-03-17 21:49:13 -0400

    Other User Profiles

---
+++ src/pages/Profile/OtherUsers/OtherUserPage.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./ProfilePage.css";
+import dp from "../../images/dp.jpg";
+import { auth } from "../../config/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { database } from "../../config/firebase";
+
+export default function OtherUserPage(props) {
+  return <div className="profile-container"></div>;
+}
