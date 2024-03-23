4b02b6b2ef2e073be9a5ee4938b3d2327550c1a5
bryanmartinez1 <bryanmartinez2401@gmail.com>
2024-03-23 15:15:17 -0400

    messaging updates

--- src/pages/MessageInbox/MessageInbox.js
+++ src/pages/MessageInbox/MessageInbox.js
@@ -1,41 +1,14 @@
 // MessageInbox.js
 
-import React, { useState } from 'react';
-import './MessageInbox.css';
+import React, { useState } from "react";
+import "./MessageInbox.css";
 
 const MessageInbox = () => {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-
-  const handleMessageChange = (e) => {
-    setMessage(e.target.value);
-  };
-
-  const handleMessageSend = () => {
-    if (message.trim() !== '') {
-      setMessages([...messages, message]);
-      setMessage('');
-    }
-  };
+  const [talkingToName, setTalkingToName] = useState("John Doe");
 
   return (
     <div className="message-container">
-      {messages.map((msg, index) => (
-        <div key={index} className="message">
-          <span className="sender">Sender:</span> {msg}
-        </div>
-      ))}
-      <div className="input-container">
-        <textarea
-          className="message-input"
-          placeholder="Type your message..."
-          value={message}
-          onChange={handleMessageChange}
-        />
-        <button className="send-button" onClick={handleMessageSend}>
-          Send
-        </button>
-      </div>
+      <div className="otherUserInfo">{talkingToName}</div>
     </div>
   );
 };
